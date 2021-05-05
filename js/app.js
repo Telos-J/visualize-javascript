566df184b5cc066b92e47ea5f930e16c52a25935
Jiseung Jung <roadrunner23@yonsei.ac.kr>
2021-05-05 20:50:52 +0900

    Add iframe output

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 require(["vs/editor/editor.main"], main);
 
 function main() {
+    let output = document.querySelector('#output').contentWindow.document
     let container = document.querySelector('#monaco-editor-container')
     let editor = monaco.editor.create(container, {
         language: 'javascript',
@@ -8,6 +9,15 @@ function main() {
         fontFamily: 'Monaco',
         scrollBeyondLastLine: false,
         showEvents: true,
-        value: `console.log('Hello World!')`,
+        value: `document.querySelector('h1').innerHTML = 'Hello World!'`,
     });
+
+    editor.onDidChangeModelContent((e) => {
+        let html = `<h1></h1>`
+        let script = editor.getValue()
+
+        output.open();
+        output.write(`${html}<script>${script}</script>`)
+        output.close();
+    })
 }
