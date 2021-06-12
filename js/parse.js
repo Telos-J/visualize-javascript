5b4f13d7c0572f8e332ed60afc2d6a9ce6730c1f
Kellyn-cloud <jipark7739@gmail.com>
2021-06-12 21:34:35 +0900

    made expression statement class

--- js/parse.js
+++ js/parse.js
@@ -8,11 +8,24 @@ class VariableDeclaration {
     }
 }
 
+class ExpressionStatement {
+    constructor(object, property, argument) {
+        this.object = object;
+        this.property = property;
+        this.argument = argument;
+    }
+}
+
 export function parseScript(script) {
     console.clear()
     let syntax = esprima.parseScript(script, { loc: true, range: true })
-    for (let node of syntax.body) {
-        if (node.type === 'VariableDeclaration') {
+
+    for (let node of syntax.body){
+        if (node.type === 'ExpressionStatement') {
+            const callee = node.expression.callee
+            console.table(new ExpressionStatement(callee.object.name, callee.property.name, node.expression.arguments[0].value))
+        }
+        else if (node.type === 'VariableDeclaration') {
             for (let declaration of node.declarations) {
                 console.table(new VariableDeclaration(node.kind, declaration.id.name, declaration.init.value))
             }
