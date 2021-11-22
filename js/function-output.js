1a8b596e73748f267f642a337012a8a2595563ad
Kellyn-cloud <jipark7739@gmail.com>
2021-11-22 21:57:22 +0900

    Start cat animation

--- js/function-output.js
+++ js/function-output.js
@@ -7,8 +7,12 @@ gsap.registerPlugin(MorphSVGPlugin)
 
 function outputConsole() {
     const fish = output.querySelector('#fish')
+    const cat = output.querySelector('#cat')
+    const head = cat.querySelector ('#head')
     gsap.set(fish, { display: 'block '})
     gsap.from(fish, { x: -500 })
+    //gsap.to(head, { rotate: -48.76, transformOrigin: "60% 100%" });
+    gsap.to(head, { rotate: -61.3, x: "-10%" , y: "28%" , transformOrigin: "60% 100%" });
 }
 
 addEventListener('load', () => {
