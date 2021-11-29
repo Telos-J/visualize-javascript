9d46c404bbcc0796c393c35246abcaf5697fe5b8
Kellyn-cloud <jipark7739@gmail.com>
2021-11-29 20:58:01 +0900

    animate cat

--- js/function-output.js
+++ js/function-output.js
@@ -9,11 +9,20 @@ function outputConsole() {
     const fish = output.querySelector('#fish')
     const cat = output.querySelector('#cat')
     const head = cat.querySelector ('#head')
+    const body = cat.querySelector('#body')
+    const tail = cat.querySelector('#tail')
+    const frontLegs = cat.querySelector('#front-legs')
+    const backLegs = cat.querySelector('#back-legs')
     gsap.set(fish, { display: 'block '})
     gsap.from(fish, { x: -500 })
     //gsap.to(head, { rotate: -48.76, transformOrigin: "60% 100%" });
     gsap.to(head, { rotate: -61.3, x: "-10%" , y: "28%" , transformOrigin: "60% 100%" });
-}
+    gsap.to(body, { rotate: -17.4, x: "-7.5%", y: "17%", transformOrigin: "center" });
+    gsap.to(tail, { rotate: -16.45, x: "-41%", transformOrigin: "0% 100%"});
+    gsap.to(frontLegs, { rotate: 58.38, x: "5%", y: "27%", transformOrigin: "top"});
+    gsap.to(backLegs, {rotate: -8.78, x: "-13%", transformOrigin: "top"});
+    }
+
 
 addEventListener('load', () => {
     output = document.querySelector('#output').contentWindow.document
