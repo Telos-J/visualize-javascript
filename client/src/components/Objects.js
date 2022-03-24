f28104c9ea0fea7ccc463232321d4672c8cc9ac5
jasonpsh <jasonpsh1306@gmail.com>
2022-03-24 20:52:05 +0900

    add background image

--- client/src/components/Objects.js
+++ client/src/components/Objects.js
@@ -1,7 +1,16 @@
 import {useState} from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    background-image: url("../img/gameBackground.jpg");
+    background-size: cover;
+    background-position: center;
+`
 
 function Objects({setOutputHandler}) {
-    return <></>
+    return <Container></Container>
 }
 
 export default Objects
